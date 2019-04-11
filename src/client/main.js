9be0fb9c51f49088e12c2ebb444ab308e0a01acf
rqcao <rqcao@outlook.com>
2019-04-11 17:09:00 +0800

    ✨ 引入iView

--- src/client/main.js
+++ src/client/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
+Vue.use(iView);
 Vue.config.productionTip = false
 axios.defaults.baseURL = "http://localhost:3000"  //设置 ajax 请求中的基础 URL
 Vue.prototype.$axios = axios
 
-axios.get("/userInfo", {  // 这里的 this 指向 Vue
-  params: {
-    ID: 12345
-  }
+
+axios.get("/api/user/listAll", {  // 这里的 this 指向 Vue
+  // params: {
+  //   ID: 12345
+  // }
 })
     .then(function(response) {
       console.log(response)
