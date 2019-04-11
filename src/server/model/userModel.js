392763d7faea272827213ebc0b7fcc129516e428
rqcao <rqcao@outlook.com>
2019-04-11 16:08:01 +0800

    增加获取用户列表的方法

--- src/server/model/userModel.js
+++ src/server/model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const logger = require('../middlewares/logger/logger')
 
 const userSchema = new Schema({
   username: String,
--- src/server/routers/api/user.js
+++ src/server/routers/api/user.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const {UserService} = require('../../services/user_service')
 const logger = require('../../middlewares/logger/logger')
-// const { MD5_SUFFIX, md5, secretKey } = require('../../config/md5_salt');
 const pbkdf2Sync = require('crypto').pbkdf2Sync
 const jwt = require('jsonwebtoken')
 // const bluebird = require('bluebird')
@@ -90,6 +89,25 @@ router.post('/login', (req, res, next) => {
         })
       }, (err) => {
         res.json({success: false, errMessage: `数据库操作失败${err}`})
+      })
+})
+
+
+/*
+ *  author: imricky
+ *  time: 2019-04-11 15:59
+ *  function: 获取用户列表
+*/
+router.get('/listAll', (req, res, next) => {
+  UserService.getAllUsers()
+      .then((data) => {
+        res.json({
+          data
+        })
+      })
+      .catch(e=>{
+        logger.error(e)
+        next(e)
       })
 })
 
