7cc9d8a592bb76ef6bec4c00d1e85d877717ce1e
rqcao <rqcao@outlook.com>
2019-05-06 20:07:52 +0800

    初步实现从后端取数据，并渲染到页面上，下一步是调整文章Card的样式，变得好看一些

--- src/client/components/ArticleAbstractCard.vue
+++ src/client/components/ArticleAbstractCard.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="list-wrapper">
-    <Card v-for="item in movieList" class="article-content">
-      <p slot="title">{{item.name}}</p>
-      <a :href="item.url" target="_blank" class="list-url">{{item.url}} </a>
-      <p>{{item.rate}}</p>
+    <Card v-for="item in articleList" class="article-content" :key="item.title">
+<!--      <p slot="title">{{item.name}}</p>-->
+<!--      <a :href="item.url" target="_blank" class="list-url">{{item.url}} </a>-->
+      <p>{{item.title}}</p>
+      <p>{{item.content}}</p>
+      <p>{{item.viewCount}}</p>
+      <p>{{item.commentCount}}</p>
+      <p>{{item.time}}</p>
+      <p>{{item.author}}</p>
+      <p>{{item.tags}}</p>
+      <p>{{item.isPublish}}</p>
     </Card>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+  import Vue from 'vue'
   /*
    *  author: imricky
    *  time: 2019-05-05 19:58
@@ -18,39 +27,23 @@
     name: "ArticleAbstractCard",
     data(){
       return {
-        movieList: [
-          {
-            name: 'The Shawshank Redemption',
-            url: 'https://movie.douban.com/subject/1292052/',
-            rate: 9.6
-          },
-          {
-            name: 'Leon:The Professional',
-            url: 'https://movie.douban.com/subject/1295644/',
-            rate: 9.4
-          },
-          {
-            name: 'Farewell to My Concubine',
-            url: 'https://movie.douban.com/subject/1291546/',
-            rate: 9.5
-          },
-          {
-            name: 'Forrest Gump',
-            url: 'https://movie.douban.com/subject/1292720/',
-            rate: 9.4
-          },
-          {
-            name: 'Life Is Beautiful',
-            url: 'https://movie.douban.com/subject/1292063/',
-            rate: 9.5
-          },
-          {
-            name: 'Spirited Away',
-            url: 'https://movie.douban.com/subject/1291561/',
-            rate: 9.2
-          }
-        ],
+        articleList: '',
       }
+    },
+    methods:{
+      getList(){
+        axios.get("/api/article/getList", {  // 这里的 this 指向 Vue
+        })
+            .then((response) => {
+              this.articleList = response.data.data
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+      }
+    },
+    mounted() {
+      this.getList()
     }
   }
 </script>
