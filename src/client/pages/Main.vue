dc6db6df7db361e618afa8f15104c2eaf3b93e05
rqcao <rqcao@outlook.com>
2019-05-09 15:33:52 +0800

    沿用昨天的页面，样式后面慢慢调试，先做其他的

--- src/client/pages/Main.vue
+++ src/client/pages/Main.vue
@@ -1,45 +1,58 @@
 <template>
-  <div>
-    <div class="Top">
-      <div class="TopContent">
-        <Row justify="start" type="flex" align="middle">
-          <Col span="12" class="TopLeft">
-            <a href="/" name="top" title="the best or nothing">
-              <div id="Logo"></div>
-              <div id="Search">
-                <Input search placeholder="Enter something..." />
-              </div>
-            </a>
-          </Col>
-          <Col span="12" class="TopRight">
-            <Button class="sign">注册</Button>
-            <Button class="sign login">登录</Button>
-          </Col>
-        </Row>
-      </div>
-    </div>
-    <div class="Wrapper">
-      <div class="content">
-        <div class="LeftBar">
+  <div class="layout">
+    <Layout>
+      <Header>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <div class="layout-logo"></div>
+          <div class="search">
+            <Input search enter-button placeholder="Enter something..." />
+          </div>
+          <div class="layout-nav">
+            <MenuItem name="1">
+              <Icon type="ios-navigate"></Icon>
+              登录
+            </MenuItem>
+            <MenuItem name="2">
+              <Icon type="ios-keypad"></Icon>
+              注册
+            </MenuItem>
+          </div>
+        </Menu>
+      </Header>
+      <Layout>
+        <Sider hide-trigger class="sider">
+          <div class="demo-avatar">
+            <Avatar shape="square" icon="ios-person" size="large"/>
+            <Avatar shape="square" icon="ios-person"/>
+            <Avatar shape="square" icon="ios-person" size="small"/>
+          </div>
+          <div class="tags">
+            <Tag color="default">default</Tag>
+            <Tag color="primary">primary</Tag>
+            <Tag color="success">success</Tag>
+            <Tag color="error">error</Tag>
+            <Tag color="warning">warning</Tag>
+            <Tag color="magenta">magenta</Tag>
+            <Tag color="red">red</Tag>
+          </div>
+        </Sider>
+        <Layout class="content-wrapper">
+          <Content class="content">
+            <ArticleAbstractCard />
+          </Content>
+        </Layout>
 
-        </div>
-        <div class="Main"></div>
-      </div>
-    </div>
-    <div class="Bottom">
-      <div class="content">
-
-      </div>
-    </div>
+      </Layout>
+      <Footer class="layout-footer-center">2019 &copy; imricky</Footer>
+    </Layout>
   </div>
 </template>
 
 <script>
   import ArticleAbstractCard from "../components/ArticleAbstractCard"
-
   export default {
     name: "Main",
-    components: {
+    components:{
       ArticleAbstractCard
     },
     data() {
@@ -51,68 +64,73 @@
 </script>
 
 <style scoped lang="scss">
-  * {
-    box-sizing: border-box;
+  .layout {
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
   }
 
-  .Top {
-    text-align: center;
-    background-color: #FFDECB;
-    height: 44px;
-    font-size: 15px;
-    font-weight: 500;
-    background-size: 44px 44px;
-    border-bottom: 1px solid rgba(0, 0, 0, .22);
-    padding: 0 20px 0 20px;
-
-    > .TopContent {
-      min-width: 600px;
-      max-width: 1100px;
-      max-width: 1060px;
-      margin: 0 auto;
-    }
-
-    .TopLeft {
-      text-align: left;
-      display: inline-block;
-      position: relative;
-      /*margin-top: 10px;*/
-    }
-    .sign{
-      float: right;
-    }
-    .sign.login{
-      margin-right: 20px;
-    }
-  }
-
-  #Logo {
-    width: 94px;
+  .layout-logo {
+    width: 100px;
     height: 30px;
-    background-image: url('../assets/logo.png');
-    background-color: #e2e2e2;
-    background-size: 94px 30px;
-    background-repeat: no-repeat;
-    display: inline-block;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
     position: relative;
-    top: 6px;
-  }
-  #Search{
-    display: inline-block;
-    position: relative;
-    bottom: 5px;
-    margin-left: 30px;
+    top: 15px;
+    left: 20px;
   }
 
-  .Wrapper{
-    text-align: center;
-    background-color: #e2e2e2;
-    .content{
-      min-width: 600px;
-      max-width: 1100px;
-      margin: 0 auto;
+  .search {
+    width: 200px;
+    height: 30px;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 13px;
+    left: 60px;
+
+    > .search-button {
+      float: left;
+      position: relative;
+      left: 205px;
+      bottom: 45px;
+
     }
   }
 
+  .content-wrapper {
+    padding: 0 24px 24px;
+
+    > .content {
+      padding: 24px;
+      min-height: 550px;
+      background: #fff
+    }
+  }
+
+  .layout-nav {
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
+  }
+
+  .sider {
+    background: #fff;
+    minHeight: '380px'
+  }
+
+  .content-container {
+    padding: '0 24px 24px';
+  }
+
+  .layout-footer-center {
+    text-align: center;
+    /*position: fixed;*/
+    /*bottom: 0px;*/
+    /*width: 100%;*/
+  }
 </style>
 
