4f355fc40422e8d13480d6e5607b03ae06c29310
rqcao <rqcao@outlook.com>
2019-05-09 21:15:54 +0800

    初步使用vue-router，脚手架里不能直接使用template，必须引入组件的形式

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
-    <!--    <img alt="Vue logo" src="./assets/logo.png">-->
-    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <Main/>
+    <h1>Hello App!</h1>
+    <p>
+      <!-- use router-link component for navigation. -->
+      <!-- specify the link by passing the `to` prop. -->
+      <!-- <router-link> will be rendered as an `<a>` tag by default -->
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </p>
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <router-view></router-view>
+    <Main />
   </div>
 </template>
 
