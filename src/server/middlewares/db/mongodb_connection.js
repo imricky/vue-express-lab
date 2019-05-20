bf9bcd80365a626527cd5714426470e7eebc7232
rqcao <rqcao@outlook.com>
2019-05-20 11:12:14 +0800

    🔨  mongodb的问题：参见： https://mongoosejs.com/docs/deprecations.html#-findandmodify-

--- src/server/middlewares/db/mongodb_connection.js
+++ src/server/middlewares/db/mongodb_connection.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const {database} = require('../../config/dbConfig')
-mongoose.connect(database.uri, {useNewUrlParser: true});
+mongoose.connect(database.uri, {useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true})
 
-const db = mongoose.connection;
+const db = mongoose.connection
 
 const logger = require('../logger/logger')
 
-db.on('open',()=>{
-  logger.info(`数据库连接成功 || 成功时间: ${Date()}`);
-});
+db.on('open', () => {
+  logger.info(`数据库连接成功 || 成功时间: ${Date()}`)
+})
 
 db.on("error", function (error) {
-  logger.error(`数据库连接失败 || 失败时间: ${Date()}`);
-});
+  logger.error(`数据库连接失败 || 失败时间: ${Date()}`)
+})
 
-module.exports = db;
+module.exports = db
