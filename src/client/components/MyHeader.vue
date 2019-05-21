5242974d26ec055d0353b4327067bf7f97a9d970
rqcao <rqcao@outlook.com>
2019-05-21 21:30:05 +0800

    ✨实现登录注册/用户名注销的切换

--- src/client/components/MyHeader.vue
+++ src/client/components/MyHeader.vue
@@ -4,34 +4,71 @@
       <div class="Logo" @click="backToHome"></div>
       <i class="Line"></i>
       <div class="Search">
-        <Input search placeholder="Enter something..." />
+        <Input search placeholder="Enter something..."/>
       </div>
-      <div class="Button">
-        <Button icon="md-log-in" class="Login-Button">登录</Button>
+      <div class="Button" v-if="!isLogin">
+        <router-link to="/signin">
+          <Button icon="md-log-in" class="Login-Button">登录</Button>
+        </router-link>
         <Button icon="md-person-add" class="Register-Button">注册</Button>
+      </div>
+      <div class="Button" v-else>
+        <router-link to="/signin">
+          <Button icon="md-log-in" class="Login-Button">{{username}}</Button>
+        </router-link>
+        <Button icon="md-person-add" class="Register-Button" @click="logout">注销</Button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import jwt from 'jsonwebtoken'
+
   export default {
     name: "MyHeader",
-    data(){
+    data() {
       return {
-
+        username: '',
       }
     },
-    methods:{
-      backToHome(){
-        this.$router.push({path: '/'})
+    computed: {
+      isLogin() {
+        if (this.username === '') {
+          return false
+        }
+        return true
       }
+    },
+    created() {
+      //获取localStorage里的jwt判断是否登录
+      this.getUsername()
+    },
+    methods: {
+      backToHome() {
+        this.$router.push({path: '/'})
+      },
+      getUsername() {
+        let token = window.localStorage.getItem('jwt_token')
+        if (token) {
+          let userInfo = jwt.decode(token)
+          console.log('执行了')
+          this.username = userInfo.username
+        }
+      },
+      //注销
+      logout() {
+        console.log(`${this.username}注销了`)
+        window.localStorage.removeItem('jwt_token')
+        this.$router.go(0) //路由刷新，但是会一瞬间白屏，体验不好
+        // location.reload() //同样的效果
+      },
     }
   }
 </script>
 
 <style scoped lang="scss">
-  #Top{
+  #Top {
 
     text-align: center;
     background-color: #fff;
@@ -39,11 +76,12 @@
     font-size: 15px;
     font-weight: 500;
     background-size: 44px 44px;
-    border-bottom: 1px solid rgba(0,0,0,.22);
+    border-bottom: 1px solid rgba(0, 0, 0, .22);
     padding: 0 20px 0 20px;
 
   }
-  .Content{
+
+  .Content {
     position: relative;
     max-width: 1100px;
     min-width: 600px;
@@ -54,33 +92,39 @@
     height: 44px;
     align-items: center;
   }
-  .Logo{
+
+  .Logo {
     width: 64px;
     height: 30px;
     background: #5b6270;
     border-radius: 3px;
     float: left;
     position: relative;
-    cursor:pointer
+    cursor: pointer
   }
-  .Line{
+
+  .Line {
     height: 29px;
     width: 1px;
     background-color: #ebebeb;
     margin-left: 17px;
   }
-  .Search{
+
+  .Search {
     margin-left: 20px;
   }
-  .Button{
+
+  .Button {
     flex: 1 1;
     justify-content: flex-end;
     display: flex;
     align-items: center;
-    .Login-Button{
+
+    .Login-Button {
       margin-right: 20px;
     }
-    .Register-Button{
+
+    .Register-Button {
       /*margin-right: 40px;*/
     }
   }
