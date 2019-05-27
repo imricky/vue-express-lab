9684b92afdd1738c5d6a256446188bc977467ac3
rqcao <rqcao@outlook.com>
2019-05-27 18:21:48 +0800

    ✨添加table动态渲染函数，渲染view和删除

--- src/client/pages/admin/ArticleManage.vue
+++ src/client/pages/admin/ArticleManage.vue
@@ -1,14 +1,100 @@
 <template>
 <div>
-  123123123
+  <Table stripe :columns="articleColumns" :data="articleData"></Table>
 </div>
 </template>
 
 <script>
   export default {
     name: "ArticleManage",
-    data() {
-      return {}
+    data () {
+      return {
+        articleColumns: [
+          {
+            title: 'Title',
+            key: 'title',
+            render: (h, params) => {
+              return h('div', [
+                h('Icon', {
+                  props: {
+                    type: 'person'
+                  }
+                }),
+                h('strong', params.row.name)
+              ]);
+            }
+          },
+          {
+            title: 'Age',
+            key: 'age'
+          },
+          {
+            title: 'Address',
+            key: 'address'
+          },
+          {
+            title: 'Action',
+            key: 'action',
+            width: 150,
+            align: 'center',
+            render: (h, params) => {
+              return h('div', [
+                h('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.show(params.index)
+                    }
+                  }
+                }, 'View'),
+                h('Button', {
+                  props: {
+                    type: 'error',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.remove(params.index)
+                    }
+                  }
+                }, 'Delete')
+              ]);
+            }
+          }
+        ],
+        articleData: [
+          {
+            name: 'John Brown',
+            age: 18,
+            address: 'New York No. 1 Lake Park',
+            date: '2016-10-03'
+          },
+          {
+            name: 'Jim Green',
+            age: 24,
+            address: 'London No. 1 Lake Park',
+            date: '2016-10-01'
+          },
+          {
+            name: 'Joe Black',
+            age: 30,
+            address: 'Sydney No. 1 Lake Park',
+            date: '2016-10-02'
+          },
+          {
+            name: 'Jon Snow',
+            age: 26,
+            address: 'Ottawa No. 2 Lake Park',
+            date: '2016-10-04'
+          }
+        ]
+      }
     },
     computed: {},
     methods: {},
