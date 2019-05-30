29761565a1165b1eb4dd8195bd7739b6bec4c7c3
rqcao <rqcao@outlook.com>
2019-05-30 17:10:03 +0800

    ✨ css实现登录背景大图

--- src/client/pages/login/signin.vue
+++ src/client/pages/login/signin.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="layout">
-    <Form ref="formInline" :model="formInline" :rules="ruleInline">
-      <FormItem prop="user">
-        <Input type="text" v-model="formInline.user" placeholder="Username">
-          <Icon type="ios-person-outline" slot="prepend"></Icon>
-        </Input>
-      </FormItem>
-      <FormItem prop="password">
-        <Input type="password" v-model="formInline.password" placeholder="Password">
-          <Icon type="ios-lock-outline" slot="prepend"></Icon>
-        </Input>
-      </FormItem>
-      <FormItem>
-        <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
-      </FormItem>
-    </Form>
+  <div id="all">
+    <div class="layout">
+      <!--      <Form ref="formInline" :model="formInline" :rules="ruleInline">-->
+      <!--        <FormItem prop="user">-->
+      <!--          <Input type="text" v-model="formInline.user" placeholder="Username">-->
+      <!--            <Icon type="ios-person-outline" slot="prepend"></Icon>-->
+      <!--          </Input>-->
+      <!--        </FormItem>-->
+      <!--        <FormItem prop="password">-->
+      <!--          <Input type="password" v-model="formInline.password" placeholder="Password">-->
+      <!--            <Icon type="ios-lock-outline" slot="prepend"></Icon>-->
+      <!--          </Input>-->
+      <!--        </FormItem>-->
+      <!--        <FormItem>-->
+      <!--          <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>-->
+      <!--        </FormItem>-->
+      <!--      </Form>-->
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -79,6 +82,28 @@
 
 <style scoped lang="scss">
   .layout {
-    position: relative;
+    background: url('../../Resources/cool-background.jpg');
+    /*让整个div固定在屏幕的最上方和最左方*/
+    position: fixed;
+    top: 0;
+    left: 0;
+    /*让整个div跟屏幕实现一模一样的大小，从而达到全屏效果，
+    而min-width是为了实现让屏幕宽度在1000px以内时，div的大小保持不变，
+    也就是说在这种情况下，缩放屏幕宽度时，图片不要缩放（只有在1000px以内才有效）*/
+    width: 100%;
+    height: 100%;
+    min-width: 600px;
+    /*整个div在HTML页面中各个层级的下方*/
+    z-index: -10;
+    zoom: 1;
+    background-color: #000;
+    /*上面这个是背景不要重复*/
+    background-repeat: no-repeat;
+    /*就是让图片随屏幕大小同步缩放，但是有部分可能会被裁切，不过不至于会露白*/
+    background-size: cover;
+    -webkit-background-size: cover;
+    -o-background-size: cover;
+    /*这句的意思就是图片的位置，居中，靠左对齐*/
+    background-position: center 0;
   }
 </style>
